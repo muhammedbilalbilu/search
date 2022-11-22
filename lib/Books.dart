1da17ac80e469bf7dbee9fe887f73e7b0c5bdfac
Bilal <bilumon4@gmail.com>
2022-11-22 18:18:17 +0530

    first commit

---
+++ lib/Books.dart
@@ -0,0 +1,22 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+class Book {
+  final String title;
+  final String url;
+  Book({
+    required this.title,
+    required this.url,
+  });
+}
+
+List<Book> allBooks = [
+  Book(title: 'title1', url: 'url1'),
+  Book(title: 'title2', url: 'url12'),
+  Book(title: 'title3', url: 'url16'),
+  Book(title: 'title4', url: 'url15'),
+  Book(title: 'title5', url: 'url14'),
+  Book(title: 'title6', url: 'url13'),
+  Book(title: 'title7', url: 'url17'),
+  Book(title: 'title8', url: 'url18'),
+  Book(title: 'title9', url: 'url19'),
+  Book(title: 'title10', url: 'url10'),
+];
